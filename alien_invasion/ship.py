49034b6e80aeb8d0b47223547a878cb9b6cd97ac
fei01010 <2025311510@stu.hit.edu.cn>
2026-03-31 17:19:37 +0800

    alien_invasion

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -15,19 +15,20 @@ class Ship:
         # 对于每艘新飞船，都将其放在屏幕底部中央
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # 在飞船的属性x中存储一个浮点数
+        self.x = float(self.rect.x)
+
         # 移动标志（飞船一开始不移动）
         self.moving_right = False
         self.moving_left = False
 
-        # 飞船的设置
-        self.ship_speed = 1.5
-
     def update(self):
         """根据移动标志调整飞船的位置"""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -=1
+        # 更新飞船的属性x的值， 而不是其外接矩形的属性x的值
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.rect.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.rect.x -= self.settings.ship_speed
 
     def blitme(self):
         """在指定位置绘制飞船"""
