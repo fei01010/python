2e0360da8e152cef26cf636b8db80d514db47414
fei <2025311510@stu.hit.edu.cn>
2026-04-30 21:45:02 +0800

    给仓库名添加链接

--- api_learning/python_repos_visual.py
+++ api_learning/python_repos_visual.py
@@ -15,9 +15,12 @@ print(f"Complete Results:{not response_dict['incomplete_results']}")
 
 # 处理有关仓库的信息
 repo_dicts = response_dict['items']
-repo_names, stars, hover_texts = [], [], []
+repo_links, stars, hover_texts = [], [], []
 for repo_dict in repo_dicts:
-    repo_names.append(repo_dict['name'])
+    repo_name = repo_dict['name']
+    repo_url = repo_dict['html_url']
+    repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+    repo_links.append(repo_link)
     stars.append(repo_dict['stargazers_count'])
 
     # 创建悬停文本
@@ -29,7 +32,7 @@ for repo_dict in repo_dicts:
 # 可视化
 title = "Most Starred Python Projects on Github"
 labels = {'x': 'Repository', 'y': 'stars'}
-fig = px.bar(x=repo_names, y=stars, title=title,
+fig = px.bar(x=repo_links, y=stars, title=title,
             labels=labels, hover_name=hover_texts)
 fig.update_layout(title_font_size=28, 
             xaxis_title_font_size=20, yaxis_title_font_size=20)
